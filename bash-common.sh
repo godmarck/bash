0df54f908cba515ec8a4473315f077496ae8b730
Marco <godmarck@users.noreply.github.com>
2024-08-29 13:05:59 +0100

    Update bash-common.sh

--- bash-common.sh
+++ bash-common.sh
@@ -19,5 +19,4 @@ export HISTFILE=~/.bash_eternal_history
 PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 EOF
 
-sed -i -e 's/^#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-systemctl restart ssh
+source ~/.bashrc
